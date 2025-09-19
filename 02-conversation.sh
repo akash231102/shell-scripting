288a161dd9987151aab250d8d608b412f3036cd3
akash <bugataakash@gmail.com>
2025-09-19 20:08:36 +0530

    variables

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "suresh :: what are you doing ramesh "
-echo "ramesh :: Hi suresh, i am eating "
-echo "suresh :: whatsup ramesh  "
-echo "ramesh :: just chill bro "
+person1=john
+person2=abraham
+
+echo "$person1 :: what are you doing $person2 "
+echo "$person2 :: Hi $person1, i am eating "
+echo "$person1 :: whatsup $person2  "
+echo "$person2 :: just chill bro "
