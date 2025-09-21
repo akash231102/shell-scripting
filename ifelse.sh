fb7517a4558c2685ce557b74c50074774e5e23ee
akash <bugataakash@gmail.com>
2025-09-21 06:07:23 +0530

    variables

---
+++ ifelse.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+time=$(date +%H)
+
+if [$time -lt 12];then
+message="good morning user"
+elif[time -lt 18];then
+message="good afteroon user"
+else
+message='good evening user"
+fi
+echo "$message"
