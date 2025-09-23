28fd9d1156047d519f430c76fe2ee5cf0ea8c01a
akash <bugataakash@gmail.com>
2025-09-23 20:52:25 +0530

    add

--- installation.sh
+++ installation.sh
@@ -2,16 +2,17 @@
 
 USER_ID=$(id -u)
 
-if [ $USER_ID ne 0 ];then
+if [ $USER_ID ne 0 ]; then
     echo "needs root access to execute the script"
     exit 1
 fi
 
 dnf install mysql -y
 
-if [ $? ne 0 ];then
+if [ $? ne 0 ]; then
     echo "installing mysql is failed"
     exit 1
 else
     echo "installing mysql is success"
 fi
+
